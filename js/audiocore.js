9c0d52a6db0c5578453e779c3acef3cd056b4da5
Matcha <x8086x@gmail.com>
2014-04-10 00:45:39 +0800

    added pooling manager for notes

--- js/audiocore.js
+++ js/audiocore.js
@@ -1,14 +1,13 @@
 (function(global){
-	var MAX_NOTES = 10;
+	var MAX_NOTES = 6;
 	var MIN_FREQ = 27.5;
 	var MAX_FREQ = 4186.01;
 	var MIN_GAIN = 0;
 	var MAX_GAIN = 1;
 
 	var NoteFactory = {
-		audioContext : new webkitAudioContext(),
 		createNote : function(){
-			var audioContext = this.audioContext;
+			var audioContext = new webkitAudioContext();
 			var oscillator = audioContext.createOscillator();
 			var gain = audioContext.createGainNode();
 			oscillator.type = 0;
@@ -29,9 +28,18 @@
 		this.destination = null;
 		this.loudness = 0;
 		this.frequency = 440;
+		this.init = false;
 	};
-	Note.prototype.play = function(){
-		this.oscillator.noteOn(0);
+	Note.prototype.play = function(freq, gain){
+		if(freq != null && gain != null){
+			this.edit(freq, gain);
+		}
+		if(this.init){
+			this.resume();
+		}else{
+			this.oscillator.noteOn(0);
+			this.init = true;
+		}
 	};
 	Note.prototype.pause = function(){
 		this.gain.disconnect();
@@ -40,7 +48,8 @@
 		this.gain.connect(this.destination);
 	};
 	Note.prototype.stop = function(){
-		this.oscillator.noteOff(0);
+		this.pause();
+		// this.oscillator.noteOff(0);
 	};
 	Note.prototype.edit = function(freq, gain){
 		if(freq > MAX_FREQ) freq = MAX_FREQ;
@@ -51,45 +60,51 @@
 		this.oscillator.frequency.value = freq;
 		this.gain.gain.value = gain;
 	};
-	// todo : rewrite the audio core
+
+	// ac is a note manager
 	var ac = {
-		notePool : [],
-		playNote : function(id, frequency, volume){
-			if(outOfBounds(id)) return;
-			var oscillator = context.createOscillator();
-			oscillator.type = 0;
-			oscillator.frequency.value = 440;
-			oscillator.connect(context.destination);
-			oscillator.start();
-			notePool[id] = oscillator;
-		},
-		modifyNote : function(id, frequency, volume){
-			if(outOfBounds(id)) return;	
-			try{
-				var oscillator = notePool[id];
-				oscillator.frequency.value = this.trimFrequency(frequency);
-			}catch(e){
-				//do nothing
+		notePool : function(){
+			var notes = [];
+			for(var i= 0; i< MAX_NOTES; i++){
+				notes.push(NoteFactory.createNote());
 			}
-		},
-		stopNote : function(id, frequency){
-			if(outOfBounds(id)) return;
-			try{
-				notePool[id].stop();
-				delete notePool[id];
-			}catch(e){
-				// do nothing
+			return notes;
+		}(),
+		usedNotesIndex : {},
+		unusedNote: function(){
+			var arr = [];
+			for(var i= 0; i< MAX_NOTES; i++){
+				arr.push(i);
 			}
+			return arr;
+		}(),
+		playNote : function(id, frequency, gain){
+			if(!id) return;
+			if(this.unusedNote.length == 0) return;
+
+			var noteNumber = this.unusedNote.pop();
+			this.usedNotesIndex[id] = noteNumber;
+
+			this.notePool[ this.usedNotesIndex[id] ].play(frequency, gain);
+			
+		},
+		modifyNote : function(id, frequency, gain){
+			if(!id || this.usedNotesIndex[id] == null) return;
+
+			var noteNumber = this.usedNotesIndex[id];
+			this.notePool[noteNumber].edit(frequency, gain);
+		},
+		stopNote : function(id){
+			if(!id || this.usedNotesIndex[id] == null) return;
+			var noteNumber = this.usedNotesIndex[id];
+
+			this.notePool[noteNumber].pause();
+
+			delete this.usedNotesIndex[id];
+			this.unusedNote.push(noteNumber);
 		},
 		stopAll : function(){
-
-		},
-		outOfBounds : function(id){
-			return id < 0 || id >= MAX_NOTES;
-		},
-		trimFrequency : function(freq){
-			if(freq > MAX_FREQ) return MAX_FREQ;
-			if(freq < MIN_FREQ) return MIN_FREQ;
+			
 		}
 	};
 
